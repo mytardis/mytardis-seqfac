bbe40c5f24c2d61d4cc2d082c47c8f6f55dd0a0c
Andrew Perry <ajperry@pansapiens.com>
2015-09-08 17:39:23 +1000

    Correctly configure NeCTAR Object Store example (StorageBoxOptions), use patched version of django-storages that can accept port as a string.

--- sequencing_facility/settings.py
+++ sequencing_facility/settings.py
@@ -11,22 +11,5 @@ SITE_TITLE = "Sequencing Facility"
 # footer text
 SPONSORED_TEXT = None
 
-WEBCACHE_FILE_PATH = '/data/cached/'
-
-# Workaround for using StorageBoxes on the NeCTAR Object Store
-# with storages.backends.s3.S3Storage
-# StorageBoxOptions can't seem to override these settings ?
-#import S3
-#S3.DEFAULT_HOST = 'swift.rc.nectar.org.au'
-#S3.PORTS_BY_SECURITY[False] = 8888
-#S3.PORTS_BY_SECURITY[True] = 8888
-
-# When using an S3-compatible object store, ensure 'storages'
-# is added to INSTALLED_APPS in the site-wide settings.py
-# When using storages.backends.s3boto.S3BotoStorage with the
-# NeCTAR Object Store
-AWS_S3_HOST = 'swift.rc.nectar.org.au'
-AWS_S3_PORT = 8888
-
 # For S3-compatible Object Stores, setup access_key, secret_key
-# and bucket_name as StorageBoxOptions
+# bucket_name, calling_format, host and port as StorageBoxOptions
