de88cba4e4418710e3ee4b452064921d590e40d6
Andrew Perry <ajperry@pansapiens.com>
2016-03-07 11:41:54 +1100

    Some reorganisation of  settings.py, adding publications forms settings.

--- sequencing_facility/settings.py
+++ sequencing_facility/settings.py
@@ -1,7 +1,13 @@
-NAME = 'Sequencing Facility'
-SITE_TITLE = 'Sequencing Facility'
 
-FACILITY_MANAGER_GROUPS = ['mhtp-facility-managers']
+# footer text
+SPONSORED_TEXT = None
+
+# For S3-compatible Object Stores, setup access_key, secret_key
+# bucket_name, calling_format, host and port as StorageBoxOptions
+
+NAME = 'Sequencing Facility'
+
+SITE_TITLE = 'Sequencing Facility'
 
 INDEX_VIEWS = {1: 'tardis.apps.sequencing_facility.views.'
                   'SequencingFacilityIndexView'}
@@ -13,8 +19,15 @@ DATASET_VIEWS = [
      'tardis.apps.sequencing_facility.views.FastqcDatasetView')
 ]
 
-# footer text
-SPONSORED_TEXT = None
+FACILITY_MANAGER_GROUPS = ['mhtp-facility-managers']
+TRASH_USERNAME = '__trashman__'
+TRASH_GROUP_NAME = '__trashcan__'
 
-# For S3-compatible Object Stores, setup access_key, secret_key
-# bucket_name, calling_format, host and port as StorageBoxOptions
+NGS_PUBLICATION_DATASET_SCHEMA = 'http://www.tardis.org.au/pub/ngs/datasets/'
+
+PUBLICATION_FORM_MAPPINGS = [
+    {'dataset_schema': r'^http://www.tardis.edu.au/schemas/ngs/project/raw_reads$',
+     'publication_schema': NGS_PUBLICATION_DATASET_SCHEMA,
+     'form_template':
+         '/static/publication-form/ngs-extra-info-dataset-template.html'},
+]
