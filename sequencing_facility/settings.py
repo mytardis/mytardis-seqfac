74258e65a6340a168137444f6583fcac2acdd920
Andrew Perry <ajperry@pansapiens.com>
2016-08-26 16:42:35 +1000

    More fixes to make make package pip installable.

--- sequencing_facility/settings.py
+++ sequencing_facility/settings.py
@@ -4,14 +4,13 @@
 
 NAME = 'Sequencing Facility'
 
-INDEX_VIEWS = {1: 'tardis.apps.sequencing_facility.views.'
-                  'SequencingFacilityIndexView'}
+INDEX_VIEWS = {1: 'sequencing_facility.views.SequencingFacilityIndexView'}
 
 DATASET_VIEWS = [
     ('http://www.tardis.edu.au/schemas/ngs/project/raw_reads',
-     'tardis.apps.sequencing_facility.views.FastqDatasetView'),
+     'sequencing_facility.views.FastqDatasetView'),
     ('http://www.tardis.edu.au/schemas/ngs/project/fastqc',
-     'tardis.apps.sequencing_facility.views.FastqcDatasetView')
+     'sequencing_facility.views.FastqcDatasetView')
 ]
 
 FACILITY_MANAGER_GROUPS = ['mhtp-facility-managers']
--- sequencing_facility/urls.py
+++ sequencing_facility/urls.py
@@ -9,19 +9,19 @@ urlpatterns = patterns('',
     # }),
 
     # url(r'^results/(?P<dataset_id>\d+)/(?P<filename>.*)$',
-    #     'tardis.apps.sequencing_facility.views.view_html_result_file'),
+    #     'sequencing_facility.views.view_html_result_file'),
 
     url(r'^report/(?P<dataset_id>\d+)/(?P<filename>.*)$',
-        'tardis.apps.sequencing_facility.views.view_fastqc_html_report'),
+        'sequencing_facility.views.view_fastqc_html_report'),
 
 
     # custom (non-tastypie) API endpoints
     url(r'^api/version$',
-        'tardis.apps.sequencing_facility.custom_api.get_version_json'),
+        'sequencing_facility.custom_api.get_version_json'),
 
     url(r'^api/trash_experiment/(?P<experiment_id>\d+)$',
-        'tardis.apps.sequencing_facility.custom_api.trash_experiment'),
+        'sequencing_facility.custom_api.trash_experiment'),
 
     url(r'^api/_delete_all_trashed/$',
-        'tardis.apps.sequencing_facility.custom_api._delete_all_trashed'),
+        'sequencing_facility.custom_api._delete_all_trashed'),
 )
